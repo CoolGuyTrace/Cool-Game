b6b3d0adf99417e6b06b121e0846abfd7b5e5dcd
Trace <christra011@alpinesd.org>
2025-03-18 09:56:44 -0600

    Better jumping and better gravity

--- main.py
+++ main.py
@@ -31,6 +31,13 @@ class Ground:
     y = 650
     size = pygame.Rect(x, y, 1080, 100)
     ground = pygame.draw.rect(Window.window, color, size)
+
+class Keys:
+    left = pygame.K_LEFT
+    right = pygame.K_RIGHT
+    up = pygame.K_UP
+    down = pygame.K_DOWN
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -38,6 +45,8 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.velocity_y = 0  
         self.gravity = 0.5
+        self.isJumping = False
+        self.isGrounded = False
     
     def moveRight(self, pixels):
         if self.rect.x + pixels <= WIDTH - self.rect.width: 
@@ -47,9 +56,24 @@ class Player(pygame.sprite.Sprite):
         if self.rect.x - pixels >= 0: 
             self.rect.x -= pixels
 
-    def jump(self, pixels):
-        if self.rect.y + pixels >= 0:
-            self.rect.y -= pixels
+    def jump(self, velocity):
+        if self.isGrounded:
+            if not self.isJumping:  
+                self.isJumping = True
+                self.isGrounded = False  
+                self.velocity_y = velocity
+
+    def update(self):  
+        # Manage jumping and falling  
+        if self.isJumping:  
+            self.rect.y += self.velocity_y  
+            self.velocity_y += self.gravity
+            #It's supposed to stop you from being able to jump while under the platform, but I'm stupid so I'm leaving it how it is
+            if self.rect.y >= left_plat_pos[1] and self.rect.x <= left_plat_pos[2] and self.rect.x >= left_plat_pos[0]:
+                self.isJumping = False
+
+            if self.velocity_y > 0:  
+                self.isJumping = False
 
     def moveDown(self, pixels):
         if self.rect.y + pixels <= (ground_pos[1]+10) - self.rect.height:
@@ -65,11 +89,13 @@ class Player(pygame.sprite.Sprite):
         if self.rect.y > ground_pos[1] - self.rect.height:  
             self.rect.y = ground_pos[1] - self.rect.height  
             self.velocity_y = 0
+            self.isGrounded = True
 
         # Check if the player is on the platform  
         if self.rect.colliderect(left_plat_pos) and self.velocity_y >= 0:  
             self.rect.y = left_plat_pos[1] - self.rect.height  
             self.velocity_y = 0
+            self.isGrounded = True
    
 
 class Objects():
@@ -103,19 +129,18 @@ while running:
         # Check for QUIT event       
         if event.type == pygame.QUIT:  
             running = False
-    
+    Player.update(player)
     Player.apply_newtons_law(player)
 
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_LEFT]:
+    pressed = pygame.key.get_pressed()
+    if pressed[Keys.left]:
         player.moveLeft(10)   
-    if keys[pygame.K_RIGHT]:
+    if pressed[Keys.right]:
        player.moveRight(10)
-    if keys[pygame.K_DOWN]: #TODO: Crouch
+    if pressed[Keys.down]: #TODO: Crouch
         player.moveDown(10)
-    if keys[pygame.K_UP]: 
-        player.jump(15)
+    if pressed[Keys.up]: 
+        player.jump(-15)
 
     Window.window.fill(Window.background_colour)
     player_list.update()
